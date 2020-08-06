676708faf3c4294f4f75016673759b773690f4ac
mkamilgok <mkamilgok@gmail.com>
2020-08-06 13:15:59 +0300

    Refactor container and presentational components also, Add styling for movies that are listed

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import "./style.css";
+import MovieCard from "./MovieCard";
 
 export default function SearchMovies(){
 
@@ -33,18 +34,7 @@ export default function SearchMovies(){
             </form>
             <div className="card-list">
                 {movies.filter(movie => movie.poster_path).map(movie => (
-                    <div key={movie.id} className="card">
-                        <img className="card--image"
-                             src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                             alt={movie.title + ' poster'}
-                        />
-                        <div className="card--content">
-                            <h3 className="card--title">{movie.title}</h3>
-                            <p><small>RELEASE DATE: {movie.release_date}</small></p>
-                            <p><small>RATING: {movie.vote_average}</small></p>
-                            <p className="card--desc">{movie.overview}</p>
-                        </div>
-                    </div>
+                    <MovieCard key={movie.id} movie={movie}/>
                 ))}
             </div>
         </div>
